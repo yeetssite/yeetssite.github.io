f7c82fa10dc342d401af5e98cdad4eb93516a3ae
Dutch Vanderlinde <itsyeetsup@hotmail.com>
2026-09-21 01:44:24 -0600

    more song player tests

--- assets/js/songplayer.js
+++ assets/js/songplayer.js
@@ -5,15 +5,15 @@
 
 // I promise you i'm not completely stupid and that im not just vibecoding some ai
 // bs here. This dumbass code is 100% human written.
-const songs = ["../sounds/concussion.mp3", "../sounds/away.mp3", "../sounds/higher_thinking.mp3"];
-const randSongIndex = Math.floor(Math.random() * songs.length);
-const randSong = songs[randSongIndex];
-console.log("Randomly selected song:");
-console.log(randSong);
 
 // Keeping it simple by copying some basic code that wraps 
 // an audio source into a function callable by a button
 function playMusic() {
+	const songs = ["../sounds/concussion.mp3", "../sounds/away.mp3", "../sounds/higher_thinking.mp3"];
+	const randSongIndex = Math.floor(Math.random() * songs.length);
+	const randSong = songs[randSongIndex];
+	console.log("Randomly selected song:");
+	console.log(randSong);
 	var music = new Audio(randSong);
 	music.play();
 }
